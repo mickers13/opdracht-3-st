706b5fed022aad5fdda2fded7d8462c29d4aa61d
mickers13 <54250862+mickers13@users.noreply.github.com>
2020-11-24 11:22:17 +0100

    Initial commit

---
+++ Assets/Scenes/Touch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : Sense
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Aspect aspect = other.GetComponent<Aspect>();
+        if (aspect != null)
+        {
+            //Check the aspect
+            if (aspect.aspectType != aspectName)
+            {
+                print("Enemy Touch Detected");
+            }
+        }
+    }
+}
